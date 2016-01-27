95de7daab5636959d1d7252ac693b1abcbd80a4a
ricardosekine <ricardo.vbsekine@gmail.com>
2016-01-26 23:21:23 -0200

    Correção Thymeleaf
    Projeto WS não usará biblioteca do Thymeleaf

--- edifacil-server/edifacil-ws/src/main/java/br/com/edifacil/spring/setup/SpringWebConfiguration.java
+++ edifacil-server/edifacil-ws/src/main/java/br/com/edifacil/spring/setup/SpringWebConfiguration.java
@@ -1,7 +1,5 @@
 package br.com.edifacil.spring.setup;
 
-import nz.net.ultraq.thymeleaf.LayoutDialect;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +14,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.thymeleaf.extras.conditionalcomments.dialect.ConditionalCommentsDialect;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @EnableWebMvc
 @Configuration
@@ -34,66 +28,66 @@ public class SpringWebConfiguration extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 
-	/**
-	 * Configuração do thymeleaf
-	 * @return {@link ServletContextTemplateResolver}
-	 */
-	@Bean
-	public ServletContextTemplateResolver templateResolver() {
-		
-		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
-		resolver.setPrefix("/WEB-INF/app/");
-		resolver.setSuffix(".html");
-		resolver.setTemplateMode("HTML5");
-		
-		if (LOGGER.isDebugEnabled()) {
-			resolver.setCacheable(false);
-		}
-		else {
-			resolver.setCacheable(true);
-		}
-		
-		resolver.setCharacterEncoding("UTF-8");
-		
-		return resolver;
-
-	}
-
-	/**
-	 * Engine com os dialetos {@link LayoutDialect} e {@link ConditionalCommentsDialect}.
-	 * @return {@link SpringTemplateEngine}
-	 */
-	public SpringTemplateEngine templateEngine() {
-		
-		SpringTemplateEngine engine = new SpringTemplateEngine();
-		engine.setTemplateResolver(templateResolver());
-		engine.addDialect(new LayoutDialect()); 
-		engine.addDialect(new ConditionalCommentsDialect());
-		
-		return engine;
-	}
-
-	/**
-	 * View resolver default da aplicação.
-	 * @return {@link ThymeleafViewResolver}
-	 */
-	@Bean
-	public ViewResolver viewResolver() {
-
-		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-		viewResolver.setTemplateEngine(templateEngine());
-		viewResolver.setOrder(1);
-		viewResolver.setViewNames(new String[]{"*"});
-		
-		if (LOGGER.isDebugEnabled()) {
-			viewResolver.setCache(false);
-		}
-		else {
-			viewResolver.setCache(true);
-		}		
-		
-		return viewResolver;
-	}
+//	/**
+//	 * Configuração do thymeleaf
+//	 * @return {@link ServletContextTemplateResolver}
+//	 */
+//	@Bean
+//	public ServletContextTemplateResolver templateResolver() {
+//		
+//		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+//		resolver.setPrefix("/WEB-INF/app/");
+//		resolver.setSuffix(".html");
+//		resolver.setTemplateMode("HTML5");
+//		
+//		if (LOGGER.isDebugEnabled()) {
+//			resolver.setCacheable(false);
+//		}
+//		else {
+//			resolver.setCacheable(true);
+//		}
+//		
+//		resolver.setCharacterEncoding("UTF-8");
+//		
+//		return resolver;
+//
+//	}
+//
+//	/**
+//	 * Engine com os dialetos {@link LayoutDialect} e {@link ConditionalCommentsDialect}.
+//	 * @return {@link SpringTemplateEngine}
+//	 */
+//	public SpringTemplateEngine templateEngine() {
+//		
+//		SpringTemplateEngine engine = new SpringTemplateEngine();
+//		engine.setTemplateResolver(templateResolver());
+//		engine.addDialect(new LayoutDialect()); 
+//		engine.addDialect(new ConditionalCommentsDialect());
+//		
+//		return engine;
+//	}
+//
+//	/**
+//	 * View resolver default da aplicação.
+//	 * @return {@link ThymeleafViewResolver}
+//	 */
+//	@Bean
+//	public ViewResolver viewResolver() {
+//
+//		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//		viewResolver.setTemplateEngine(templateEngine());
+//		viewResolver.setOrder(1);
+//		viewResolver.setViewNames(new String[]{"*"});
+//		
+//		if (LOGGER.isDebugEnabled()) {
+//			viewResolver.setCache(false);
+//		}
+//		else {
+//			viewResolver.setCache(true);
+//		}		
+//		
+//		return viewResolver;
+//	}
 
 	@Bean
 	public LocaleResolver localeResolver() {
