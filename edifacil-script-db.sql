52a3ae0c420c6b2c9c3a91c2b3f0564c7033dc9e
ricardosekine <ricardo.vbsekine@gmail.com>
2016-01-29 00:40:02 -0200

    Update MER and Script DB

---
+++ edifacil-script-db.sql
@@ -0,0 +1,97 @@
+-- MySQL Workbench Synchronization
+-- Generated: 2016-01-29 00:37
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: rsekine
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+CREATE TABLE IF NOT EXISTS `edifacil`.`authority` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `role` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `authority_to_user_fk`
+    FOREIGN KEY (`id`)
+    REFERENCES `edifacil`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `edifacil`.`booking` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `booking_type_id` INT(11) NOT NULL,
+  `start_date` DATETIME NOT NULL,
+  `end_date` DATETIME NOT NULL,
+  `creation_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `booking_to_user_idx` (`user_id` ASC),
+  INDEX `booking_to_booking_type_idx` (`booking_type_id` ASC),
+  CONSTRAINT `booking_to_booking_type`
+    FOREIGN KEY (`booking_type_id`)
+    REFERENCES `edifacil`.`booking_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `booking_to_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `edifacil`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `edifacil`.`booking_type` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `edifacil`.`occurrence` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `occurrence_type_id` INT(11) NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
+  `creation_date` DATETIME NOT NULL,
+  `status` CHAR(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `occurrence_to_user_fk_idx` (`user_id` ASC),
+  INDEX `occurrence_to_occurrence_type_fk_idx` (`occurrence_type_id` ASC),
+  CONSTRAINT `occurrence_to_occurrence_type_fk`
+    FOREIGN KEY (`occurrence_type_id`)
+    REFERENCES `edifacil`.`occurrence_type` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `occurrence_to_user_fk`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `edifacil`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `edifacil`.`occurrence_type` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `edifacil`.`user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
